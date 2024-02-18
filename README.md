fcbcdaf4cb7e33b52b8a43f2f22299cb365ae149
micnekr <misha.nekrasov@gmail.com>
2024-02-18 10:46:38 +0000

    Moved computer vision scripts to rbx1_computer_vision, and added, but not set up, darknet_ros

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # RobotArmPackages
 ROS Packages related to the OxRAM Robot Arm project.
 
-To use, `git clone` this repo into the `src` folder of your workspace.
+To use, `git clone --recursive https://github.com/OxRAMSociety/RobotArmPackages.git` into the `src` folder of your workspace.
 
 ## Installation:
 ### Computer Vision
--- cv_setup.sh
+++ cv_setup.sh
@@ -3,9 +3,15 @@
 # This file sets up the development environment for computer vision
 # This only needs to be run once before starting to work on computer vision
 
+# Make sure the submodules are installed
+git submodule update --init --recursive
+
 #Install usb camera drivers
 sudo apt-get install ros-noetic-usb-cam
 
 # Copy the calibration file to the correct place
 mkdir -p ~/.ros/camera_info/
 cp ./calibration.yaml ~/.ros/camera_info/head_camera.yaml
+
+# Build darknet_ros in release mode
+catkin build darknet_ros -DCMAKE_BUILD_TYPE=Release
