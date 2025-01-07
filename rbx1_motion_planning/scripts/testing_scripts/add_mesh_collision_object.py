819e1284f6ec0ba575799061120e5c7cc63d4fbf
Thomas-August <tommy.august@btinternet.com>
2025-01-07 17:26:51 +0000

    adjusted the add_mesh_collisio_object.py to allow for user input for a chess piece to add in or to loop through all available pieces

--- rbx1_motion_planning/scripts/testing_scripts/add_mesh_collision_object.py
+++ rbx1_motion_planning/scripts/testing_scripts/add_mesh_collision_object.py
@@ -17,27 +17,36 @@ def main():
     # Wait for the scene to initialize
     rospy.sleep(2)
 
+    # For asking user to provide a file name
+    # piece_name = str(input("Piece Name: "))
+
     # Get the package path using rospkg
     rospack = rospkg.RosPack()
     package_path = rospack.get_path('rbx1_motion_planning')  # Replace with your package name
 
-    # Define the STL file path
-    mesh_file_path = os.path.join(package_path, "meshes", "chess_board.stl")
+    piece_names = ("chess_board", "king", "queen", "bishop", "knight", "rook", "pawn")
+    for piece_name in piece_names:
 
-    # Define the pose of the collision object
-    collision_pose = geometry_msgs.msg.PoseStamped()
-    collision_pose.header.frame_id = 'world'
-    collision_pose.pose.position.x = 1.0
-    collision_pose.pose.position.y = 0.0
-    collision_pose.pose.position.z = 0.0
-    collision_pose.pose.orientation.w = 1.0  # Identity rotation
-    rospy.loginfo(collision_pose)
+        # Get file name from piece name
+        file_name = piece_name + ".stl"
 
-    # Add the mesh as a collision object in the planning scene
-    scene.add_mesh("chess_board", collision_pose, mesh_file_path)
+        # Define the STL file path
+        mesh_file_path = os.path.join(package_path, "meshes", "scaled_stl", file_name)
 
-    # Allow RViz to update and visualize the object
-    rospy.sleep(2)
+        # Define the pose of the collision object
+        collision_pose = geometry_msgs.msg.PoseStamped()
+        collision_pose.header.frame_id = 'world'
+        collision_pose.pose.position.x = 0.55
+        collision_pose.pose.position.y = 0.0
+        collision_pose.pose.position.z = 0.0
+        collision_pose.pose.orientation.w = 1.0  # Identity rotation
+        rospy.loginfo(collision_pose)
+
+        # Add the mesh as a collision object in the planning scene
+        scene.add_mesh(piece_name, collision_pose, mesh_file_path, (1.5,1.5,1.5) )
+
+        # Allow RViz to update and visualize the object
+        rospy.sleep(2)
 
 if __name__ == "__main__":
     main()
